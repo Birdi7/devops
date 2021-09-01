ad9e9d1401cb8947c82a5d962a8c23a15cc68a22
Egor <egor.osokin7@gmail.com>
2021-09-01 23:13:35 +0300

    jnks

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -1,3 +1,1 @@
-#RUN FROM JENKINS SUBFOLDER
-
 docker run --rm --name jenkins -p 8080:8080 -p 50000:50000 -u 0 -v `pwd`/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
