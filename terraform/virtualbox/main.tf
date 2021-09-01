cf329d29b650e669208d626749fb27ed61f6337a
Egor <egor.osokin7@gmail.com>
2021-09-01 22:48:22 +0300

    vagrant

---
+++ terraform/virtualbox/main.tf
@@ -0,0 +1,17 @@
+// doesn't quite work
+// https://github.com/terra-farm/terraform-provider-virtualbox/issues/105
+// but let's leave it here
+
+# resource "virtualbox_vm" "node" {
+#   count     = 2
+#   name      = format("node-%02d", count.index + 1)
+#   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+#   cpus      = 2
+#   memory    = "512 mib"
+#   user_data = file("${path.module}/user_data")
+
+#   network_adapter {
+#     type           = "hostonly"
+#     host_interface = "vboxnet1"
+#   }
+# }
